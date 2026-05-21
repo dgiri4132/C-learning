ff85c01a100131379e1c17be69dd0488e0e45ef1
Darshan Giri <darshangiri@Darshans-MacBook-Pro.local>
2026-05-21 18:53:46 -0500

    Day 01 - Add review notes

--- day-01-hello-world/day1.cpp
+++ day-01-hello-world/day1.cpp
@@ -2,6 +2,45 @@
 #include <iostream>
 
 int main(){
-    std:: cout << "Hello World/n";
+    std:: cout << "Hello World\n";
     return 0;
 }
+/*Review questions answers, most relevant
+1- the purpose is to learn the simplest of codes.
+
+2- function has four parts:- return type, name (here it is main),
+    paramenter list enclosed in parentheses and a function body:-
+    which lists the actions
+
+3- main
+
+4- to check if the program executed or not
+
+5- to make the language understandable by the computer
+
+6- to bring in a library
+
+7-if import does not work, it is an older way to import stuff
+
+8- it helps the computer compiler know that the code was written in cpp
+
+9- Linker combines two object files into one executable file
+
+10- source file is what we write, object code is what is understandable by the computer
+
+11- Executable is the final output file 
+
+12- IDE is Integrated Development Environment, it bundles everything you need to 
+    write, build, and debug code into one place
+    
+13- By clicking the run button in VSCode at least
+
+14- repetition is the name of the game.
+
+-function is a sequence of instructions for the computer to be executed
+-executable can directly be run as a program.
+-command line is terminal
+-module is a self-contained unit of code.
+- header file is a file that contains declerations.
+
+*/
