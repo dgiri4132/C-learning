02e184bea6d8bf4db14c19e39648de89398020fb
Darshan Giri <darshangiri785@gmail.com>
2026-05-24 21:59:56 -0500

    Day 02 - Exercises

--- day-02-object-types-and-values/part2-object-types.cpp
+++ day-02-object-types-and-values/part2-object-types.cpp
@@ -41,3 +41,33 @@ int main(){
         cout<< "\n Are you retired ? ";
 }
 // Names can start with a letter and contain only letters, digits , and underscores
+/* Review Questions
+1- It is a command
+2- <<
+3- we can use =, (), {}
+4- cout << "Enter a number: "; cin>> number;
+5- endlines, it ends line
+6- Whitespace
+7- any non-numerical character.
+8- cout << "Hello "<< first_name << "\n";
+9- a variable is a named memory that holds a value of a given type.
+10- As is value is literal.
+11- same amount as there are variables.
+12- strutred command that gives a name and a type to an object.
+13- bytes and bits for char specific, string it depends
+14-bytes and bits
+15- = is used when assigning a value and == is used when comparing values
+16- definition is a decleration that sets aside memory for an object
+17- initialization is initial setiing of the value of the variable while
+     assignment can change the initial value.
+18- string concatenation is " adding" string and you do it with + operator.
+19- All given in the book, the unique one would be mode.
+20- c1,d1,a2,b3,a3,c3
+21- int, string, bool, char, double
+22- short and also using underscore for word separation
+23- being safe for initialization, should prevent some errors
+24- round down only, accuracy lost, narrowing
+25- information lost should be the line to draw.
+26- use {} while initializing
+27- automatic initialization
+*/
