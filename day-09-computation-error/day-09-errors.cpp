eb518b4f40f5c45778028df2b1d717ed9e48b816
Darshan Giri <darshangiri785@gmail.com>
2026-06-01 19:42:34 -0500

    Day 09- Started errors

--- day-09-computation-error/day-09-errors.cpp
+++ day-09-computation-error/day-09-errors.cpp
@@ -49,3 +49,63 @@ eg:- take line int area(int length, int width)
 }
 Int s3=area(7,2); all of them showed easy error signals*/
 
+// Type errors
+/* 
+Using same things as examples
+int x0=arena(7,2);
+int x1=area(7);
+int x2=area('seven',2);
+
+answers like 
+    int x4=area(10,-7);
+    int x5=area(10.7,9.3);
+    char x6=area(100,9999)
+    
+    All of them give answers, even though there can't be a rectangle
+    with negative width or the numbers will be rounded  down implicitly
+    */
+
+
+// Link-time errors
+
+/* A program consists of several compiled parts, called translation units
+or modules. Every function in a program must be declared with eactly the same type in every translation unit in which it is used. 
+Every function must also be defined exactly once in a program.
+
+here follows a typical linker error
+
+int area(int length, int width);
+
+int main()
+{int x = area(2,3);}
+
+This code above will give error because the function 
+is only called but not defined.
+Functions with the same name but different types will not match and will be ignored.
+*/
+
+// Run-time errors
+
+/* The output produced is wrong, maybe logic is flawed or maybe didn't write what you thought you wrote
+maybe made a silly error or whatever
+
+eg;-
+
+int area(int length, int width)
+{
+return length*width;
+}
+
+int framed_area(int x,int y)
+{
+return area(x-2,y-2);
+}
+
+void test(int, int y , int z){
+    int area1=area(x,y)
+    int area2=framed_area(1,z);
+    int area3=framed_area(y,z);
+    double ratio=double(area1)/area3;
+    }
+    Here we know that int by int is int so you need to convert
+    area, which is in int to double so that the ratio is in double*/
