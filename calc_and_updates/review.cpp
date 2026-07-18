9938a148e731c26dc315a261e956c55685c7e68a
Darshan Giri <darshangiri785@gmail.com>
2026-07-18 05:41:13 -0500

    Finished the review questions and terms, moving on to drills tommorow

---
+++ calc_and_updates/review.cpp
@@ -0,0 +1,10 @@
+/*
+Here are the review answers for the chapter
+(1)- debugging, improving the version, edge cases
+(2)- Because the error will end the program and it does not go till quit
+(3)- Because it would be easy to use in whole of the program.
+(4)- Because we need to address two different statements , either a calculation or a 
+    variable declaration and doing that in the main would be messy.
+(5)- Because it woudl be easier to debugg and to edit and also to add feature later on.
+(6)- To let users and programmers know what we have made changes on or what we are doing in the particular section.
+(7)- */
