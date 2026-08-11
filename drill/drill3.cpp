f7cc3d8885217ce2688182a367f2c7a2d1ca3141
Darshan Giri <darshangiri785@gmail.com>
2026-08-11 16:09:32 -0500

    Just started working on how to write modules

---
+++ drill/drill3.cpp
@@ -0,0 +1,11 @@
+module;
+#include "std_lib_facilities.h"
+export module foo;
+
+
+export void print_foo(){
+    int foo = 0;
+    std ::cout << "foo is "<< 0;
+};
+export void set_foo(int x){};
+export int get_foo(){return x;}
