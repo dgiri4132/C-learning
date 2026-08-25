46829b44c69ea6cc748a9663d25b05a172ed6f12
Darshan Giri <darshangiri785@gmail.com>
2026-08-24 21:13:07 -0500

    Doing sorting name question finally finished

--- drill/exercise6.cpp
+++ drill/exercise6.cpp
@@ -22,15 +22,36 @@ vector<string> reversed(vector<string>add){
     return add;
 }
 
+string lowered(string ld){
+    string l = "";
+    for (int i = 0;i<l.size();++i){
+        l+=tolower(ld[i]);
+    }
+    return l;
+}
+
+
 vector<string> ordering(vector<string> name){
     vector<double> ages = {};
     double age = 0;
-    for (int i = 0; i <5;++i){
+    for (int i = 0; i < name.size(); ++i){
         cout << "Enter the respective age: "<< "\n";
         cin >> age;
         ages.push_back(age);
     }
-    vector<string>copied = name;
-    
 
+    for(int i = 0; i<name.size();++i){
+        for (int j = i+1;j < name.size();++j){
+            if (lowered(name[j])<lowered(name[i])){
+                swap(name[i],name[j]);
+                swap(ages[i],ages[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < name.size(); ++i){
+        cout << name[i] << " " << ages[i] << "\n";
+    }
+
+    return name;
 }
